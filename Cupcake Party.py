b95aed7c6dca1e26d76d779958fb240f8a81bafd
Shreerama Mishra <88994493+Shreerama-Mishra@users.noreply.github.com>
2022-02-22 19:09:27 -0500

    Add files via upload

---
+++ Cupcake Party.py
@@ -0,0 +1,11 @@
+# regular holds 8 cupcakes
+# small holds 3 cupcakes
+r = int(input())
+s = int(input())
+
+total = (r*8 + s*3) - 28
+if total == 28:
+    total / 28
+output = total 
+
+print(output)
